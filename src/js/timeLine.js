4b1ea042abdbe093b01a4cee8af4cfda7a70cfb6
Brenda Sánchez <sanrey254@hotmail.com>
2018-07-23 17:16:12 -0500

    Agregando funcionalidad para editar publicaciones

--- src/js/timeLine.js
+++ src/js/timeLine.js
@@ -7,6 +7,7 @@ document.getElementById('sign-out').addEventListener('click', event =>{
   socialNetwork.signOut();
 })
 
+
 const setUserProfile = user =>{
   document.getElementById('current-user-name').innerHTML = user.displayName;
   document.getElementById('current-user-email').innerHTML = user.email;
@@ -69,9 +70,9 @@ const drawPostByUser = () =>{
         //console.log(userInfo);
         result += `<div class="card mb-4 border-secondary">
           <div class="card-body">
-            <p class="card-text">${post.data().content}</p>
-          </div><div class="card-header small-font"><div class="container"><div class="row"><div class="col-md-8"><div class="row"><div class="col-md-2 pr-0 col-2"><img src="${post.data().userPhoto}" class="rounded-circle profile-image"></div><div class="col-10 col-md-10 pl-0"><strong>${post.data().userName}</strong><p>${post.data().time}</p></div></div></div><div class="col-md-4 text-md-right text-center">${post.data().likes} <button class="no-btn mr-4" onclick="addLikeToPost('${post.id}')"><i class="fas fa-thumbs-up"></i></button>
-          <button class="no-btn" onclick="deletePost('${post.id}')"><i class="far fa-trash-alt"></i></button><button class="no-btn" onclick="updatePost('${post.id}')"><i class="ml-3 fas fa-pencil-alt"></i></button></div></div></div>
+            <p class="card-text" id="${post.id}">${post.data().content}</p>
+          </div><div class="card-header small-font"><div class="container"><div class="row"><div class="col-md-8"><div class="row"><div class="col-md-2 px-0 px-md-2 col-2"><img src="${post.data().userPhoto}" class="rounded-circle profile-image"></div><div class="col-10 col-md-10 pl-0"><strong>${post.data().userName}</strong><p>${post.data().time}</p></div></div></div><div class="col-md-4 text-md-right text-center">${post.data().likes} <button class="no-btn mr-4" onclick="addLikeToPost('${post.id}')"><i class="fas fa-thumbs-up"></i></button>
+          <button class="no-btn" onclick="deletePost('${post.id}')"><i class="far fa-trash-alt"></i></button><button class="no-btn" onclick="createUpdateArea('${post.id}')"><i class="ml-3 fas fa-pencil-alt"></i></button></div></div></div>
           </div>
         </div>`;
         i++;
@@ -113,9 +114,29 @@ const deletePost = (postID) =>{
     });
 };
 
-const updatePost = (postID) =>{
-
+const createUpdateArea = postID =>{
+  db.collection('post').doc(postID).get()
+  .then(post => {
+    document.getElementById(postID).innerHTML = `<textarea class="form-control form-textarea" id="post${postID}" rows="4">${post.data().content}</textarea><div class="ml-auto text-right"><button class="btn btn-warning" onclick="updatePostContent('${postID}')"><i class="fas fa-save"></i></button><div>`;
+  }).catch(error => {
+    console.log('Error al editar');
+  })
 };
+
+const updatePostContent = postID => {
+  const postContent = document.getElementById(`post${postID}`).value;
+  console.log(postContent);
+   db.collection('post').doc(postID).get()
+    .then(post => {
+      db.collection('post').doc(postID).update({
+        content: postContent
+      }).then(element => {
+        drawPostByUser();
+      }).catch(element => {
+        console.log('Error al aumentar contador de likes');
+      });
+    });
+}
 
 
 getCurrentUserData();
